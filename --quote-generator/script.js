86b22be9e2d61b483b3ed9dd8e600af61532bcd7
Mohammed Fachihul Alam <fachirbl@gmail.com>
2024-05-16 11:21:54 +0600

    h

--- --quote-generator/script.js
+++ --quote-generator/script.js
@@ -1,1 +1,39 @@
-console.log("Hello world");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+// Show New Quote
+function newQuote(params) {
+    // Pick a random quote from api quotes
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if author field is blank and replace it with "Unknown"
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine styling
+    if (quote.text.length > 50) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+}
+// Get Quotes From API
+async function getQuote() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch error
+    }
+}
+
+// On load
+getQuote();
